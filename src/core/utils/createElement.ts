0c1e8cc34b12ea0e0240f993f3fccd13278b5beb
xunji <118670760+XunJiJiang@users.noreply.github.com>
2024-10-12 20:21:12 +0800

    fix(xj-web): #8 (#9)

--- src/core/utils/createElement.ts
+++ src/core/utils/createElement.ts
@@ -1,17 +1,20 @@
-const createElement = (
+const createElement = <T extends Node>(
   tag: string,
   opt?: { [key: string]: string },
-  children?: HTMLElement[]
+  children?: T[] | string
 ): HTMLElement => {
   const el = document.createElement(tag)
   if (opt)
     for (const key in opt) {
       el.setAttribute(key, opt[key])
     }
-  if (children)
+  if (Array.isArray(children)) {
     children.forEach((child) => {
       el.appendChild(child)
     })
+  } else if (children) {
+    el.appendChild(document.createTextNode(children))
+  }
   return el
 }
 
