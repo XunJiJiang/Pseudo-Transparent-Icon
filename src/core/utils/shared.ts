288a2492f3d5158fb5cab2d06d6a02cfa87f25fd
xunjijiang <329453626x@gmail.com>
2024-10-13 22:37:49 +0800

    feat(xj-web): 工具函数isObject，hasOwn

--- src/core/utils/shared.ts
+++ src/core/utils/shared.ts
@@ -1,13 +1,19 @@
+// /* eslint-disable @typescript-eslint/no-explicit-any */
+
 export const isFunction = (
   fn: unknown
 ): fn is (...args: unknown[]) => unknown => typeof fn === 'function'
 
 export const isArray = (arr: unknown): arr is unknown[] => Array.isArray(arr)
 
-export const hasOwn = (
+export const isObject = (val: unknown): val is object => {
+  return val !== null && typeof val === 'object' && !Array.isArray(val)
+}
+
+export const hasOwn = <T extends object>(
   val: object,
   key: string | symbol
-): key is keyof typeof val => Object.prototype.hasOwnProperty.call(val, key)
+): val is T => Object.prototype.hasOwnProperty.call(val, key)
 
 export const isHTMLElement = (el: unknown): el is HTMLElement =>
   el instanceof HTMLElement
