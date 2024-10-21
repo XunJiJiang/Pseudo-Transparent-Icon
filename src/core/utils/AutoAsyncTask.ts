2bc1d240ca3f32b06e5d2c8d374d72183491d415
xunjijiang <329453626x@gmail.com>
2024-10-21 17:11:57 +0800

    fix(xj-web.AutoAsyncTask): 修复运行异步任务时添加任务可能出现的问题

--- src/core/utils/AutoAsyncTask.ts
+++ src/core/utils/AutoAsyncTask.ts
@@ -1,27 +1,39 @@
 import { Func } from '@type/function'
 
+enum State {
+  Pending,
+  Running
+}
+
 export default class AutoAsyncTask {
-  private static tasks = new Map<Func, Func>()
+  private static taskLists = [new Map<Func, Func>(), new Map<Func, Func>()]
   private static promise: Promise<void> | null = null
+  private static state = State.Pending
 
   static addTask(task: Func, key?: Func) {
-    if (this.tasks.has(key ?? task)) this.tasks.delete(key ?? task)
-    this.tasks.set(key ?? task, task)
-    this.run()
+    const taskList = this.taskLists[this.state]
+    if (taskList.has(key ?? task)) taskList.delete(key ?? task)
+    taskList.set(key ?? task, task)
+    if (this.state === State.Pending) this.run()
   }
 
   private static run() {
+    const taskList = this.taskLists[0]
     if (this.promise) return
     this.promise = Promise.resolve()
     this.promise
       .then(() => {
-        this.tasks.forEach((task, key, map) => {
+        this.state = State.Running
+        taskList.forEach((task, key, map) => {
           task()
           map.delete(key)
         })
       })
       .finally(() => {
+        this.state = State.Pending
         this.promise = null
+        this.taskLists.reverse()
+        if (this.taskLists[0].size) this.run()
       })
   }
 }
