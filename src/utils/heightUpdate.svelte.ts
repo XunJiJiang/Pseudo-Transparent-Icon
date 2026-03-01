5ad78e3b20c0e807d9c58a478ba7777ed406cb06
XunJi <118670760+XunJiJiang@users.noreply.github.com>
2026-03-01 22:50:57 +0800

    fix: 降低了问题 "在过渡过程中发生高度变化, 可能导致最终高度设置有误" 发生的概率

--- src/utils/heightUpdate.svelte.ts
+++ src/utils/heightUpdate.svelte.ts
@@ -1,18 +1,18 @@
 /**
  * 虽然可以使用 ResizeObserver 来监听组件中高度可变部分的高度变化
- * 为了防止在某些场景下，可能触发频繁的高度变化事件，导致性能问题，因此设置了防抖
+ * 为了防止在某些场景下, 可能触发频繁的高度变化事件, 导致性能问题, 因此设置了防抖
  * 这导致可能会出现高度更新不及时的问题
- * 为了对不会频繁触发高度变化事件的场景进行优化，提供 publishHeightUpdate 用于立刻响应高度更新事件
+ * 为了对不会频繁触发高度变化事件的场景进行优化, 提供 publishHeightUpdate 用于立刻响应高度更新事件
  */
 
 import { debounce } from 'es-toolkit'
 import type { EventBus } from './eventBus'
 
 /**
- * @param eventBus 事件总线实例，用于订阅和发布高度更新事件
- * @param depth 当前组件的深度，根组件深度为 0
- * @param getRenderState 获取当前组件中高度可变部分的内容的渲染状态(是否渲染了内容)，用于判断子组件是首次渲染还是更新高度
- * @param recalculateHeight 重新计算当前组件的高度，参数为可选对象，包含 height 和 changeHeight 两个可选属性，height 表示当前组件可变高度部分的最新高度，changeHeight 表示当前组件可变高度部分的高度变化值
+ * @param eventBus 事件总线实例, 用于订阅和发布高度更新事件
+ * @param depth 当前组件的深度, 根组件深度为 0
+ * @param getRenderState 获取当前组件中高度可变部分的内容的渲染状态(是否渲染了内容), 用于判断子组件是首次渲染还是更新高度
+ * @param recalculateHeight 重新计算当前组件的高度, 参数为可选对象, 包含 height 和 changeHeight 两个可选属性, height 表示当前组件可变高度部分的最新高度, changeHeight 表示当前组件可变高度部分的高度变化值
  * @param getHeightContainer 获取高度容器, 这个容器的高度必须完全由内部元素的高度决定, 如果没有这个容器, 则传入 null 或者不传入
  * @returns
  */
@@ -20,7 +20,7 @@ export function useHeightUpdateSubscriber(
   eventBus: EventBus,
   depth: number,
   /**
-   * 获取当前组件中高度可变部分的内容的渲染状态(是否渲染了内容)，用于判断子组件是首次渲染还是更新高度
+   * 获取当前组件中高度可变部分的内容的渲染状态(是否渲染了内容), 用于判断子组件是首次渲染还是更新高度
    */
   getRenderState: () => boolean,
   recalculateHeight: (info?: { height?: number; changeHeight?: number }) => void,
@@ -28,12 +28,13 @@ export function useHeightUpdateSubscriber(
 ) {
   /** 当前组件可变高度部分的高度 */
   let currentHeight = 0
-  /** 定时器 ID，用于清除定时器 */
+  /** 定时器 ID, 用于清除定时器 */
   let interval: number
   /** 子组件高度变化时的上次的内容渲染状态*/
   let lastIsRender: boolean
   /** 设置为 true 时, 标记本次高度更新由子组件主动触发, 不需要 ResizeObserver 处理 */
   let activeUpdate = false
+  let activeUpdateTimeout: number
   // 如果上次为关闭, 本次为开启, 说明子组件首次渲染
   // 此时若子组件的建议高度更新延时(timeout)为 0, 说明子组件是通过设置 auto 来立刻获取高度
   // 由于当前组件此时通过设置 auto 来获取高度, 此时获取到的高度就是子组件为 auto 时的高度, 即正确的高度
@@ -41,7 +42,7 @@ export function useHeightUpdateSubscriber(
   // 如果上次为开启, 本次为开启, 说明子组件更新了高度
   // 此时当前组件没有触发过设置 auto 来获取高度, 获取到的高度不正确, 需要通过 changeHeight 来增量更新当前组件的高度
   $effect(() => {
-    // 监听来自子组件的高度更新事件，参数为子组件的深度
+    // 监听来自子组件的高度更新事件, 参数为子组件的深度
     const unlisten = eventBus.subscribe(
       `height-update`,
       (_depth: number, changeHeight?: number, timeout = 0, times = 1) => {
@@ -66,9 +67,13 @@ export function useHeightUpdateSubscriber(
 
         clearInterval(interval)
 
-        // 如果 changeHeight 已经提供，说明子组件已经计算出了高度变化值，可以直接使用这个值来更新当前组件的高度，而不需要等待 ResizeObserver 的回调
+        // 如果 changeHeight 已经提供, 说明子组件已经计算出了高度变化值, 可以直接使用这个值来更新当前组件的高度, 而不需要等待 ResizeObserver 的回调
         if (changeHeight !== void 0) {
+          clearTimeout(activeUpdateTimeout)
           activeUpdate = true
+          activeUpdateTimeout = setTimeout(() => {
+            activeUpdate = false
+          }, 300)
           recalculateHeight({ changeHeight })
           if (depth > 0) {
             eventBus.publish(`height-update`, depth, changeHeight, timeout, times)
@@ -87,7 +92,11 @@ export function useHeightUpdateSubscriber(
         let executedTimes = 0
 
         interval = setInterval(() => {
+          clearTimeout(activeUpdateTimeout)
           activeUpdate = true
+          activeUpdateTimeout = setTimeout(() => {
+            activeUpdate = false
+          }, 300)
           recalculateHeight()
           executedTimes++
           if (depth > 0) {
@@ -112,13 +121,22 @@ export function useHeightUpdateSubscriber(
     }
   }
 
+  // BUG: 当正在过渡过程中时又发生了高度变化, 可能会导致过渡结束时高度设置不正确
+  // 目前的解决方案是, 在所有过渡结束后再进行一次高度更新, 确保高度设置正确
+  // 因为元素过渡由 recalculateHeight 函数触发, 时常为 300ms
+  // 因此设置一个防抖在最后触发一次高度更新, 确保在过渡结束后进行一次高度更新, 确保高度设置正确
+  // 这只能一定程度上缓解问题, 无法完全解决问题, 依然有可能出现高度设置不正确的情况
+  const debouncedRecalculateHeight = debounce(
+    (height: number) => {
+      console.log('debouncedRecalculateHeight', height)
+      recalculateHeight()
+    },
+    500,
+    { edges: ['trailing'] }
+  )
+
   const debouncedPublishHeightUpdate = debounce(
     (entry: ResizeObserverEntry) => {
-      // 如果 activeUpdate 为 true，表示本次高度更新是由子组件主动触发的，不需要 ResizeObserver 处理
-      if (activeUpdate) {
-        activeUpdate = false
-        return
-      }
       const newHeight =
         entry.contentRect.height +
         (parseFloat(getComputedStyle(entry.target).paddingTop) || 0) +
@@ -128,10 +146,14 @@ export function useHeightUpdateSubscriber(
         (parseFloat(getComputedStyle(entry.target).marginTop) || 0) +
         (parseFloat(getComputedStyle(entry.target).marginBottom) || 0)
       const oldHeight = currentHeight
-      if (newHeight !== oldHeight) {
-        const change = newHeight - oldHeight
-        recalculateHeight({ changeHeight: change })
+      if (newHeight === oldHeight) {
+        return
+      }
+      const change = newHeight - oldHeight
+      if (newHeight) {
+        recalculateHeight({ height: newHeight })
         publishHeightUpdate(change)
+        debouncedRecalculateHeight(newHeight)
       }
     },
     30,
@@ -139,6 +161,10 @@ export function useHeightUpdateSubscriber(
   )
 
   const resizeObserver = new ResizeObserver((entries) => {
+    // 如果 activeUpdate 为 true, 表示本次高度更新是由子组件主动触发的, 不需要 ResizeObserver 处理
+    if (activeUpdate) {
+      return
+    }
     const mainContentElement = getHeightContainer ? getHeightContainer() : null
     if (!mainContentElement) {
       return
@@ -150,6 +176,9 @@ export function useHeightUpdateSubscriber(
     }
   })
 
+  /** 定时标志, 在元素进入页面时, 重新观察元素 */
+  let reObserveTimeout: number
+  // 监听当前组件中高度可变部分的内容的渲染状态变化, 来决定是否观察元素的高度变化
   $effect(() => {
     const mainContentElement = getHeightContainer ? getHeightContainer() : null
     const open = getRenderState()
@@ -157,20 +186,13 @@ export function useHeightUpdateSubscriber(
       return
     }
     if (open) {
-      setTimeout(() => {
-        if (!mainContentElement || !resizeObserver) {
-          return
-        }
+      reObserveTimeout = setTimeout(() => {
         resizeObserver.observe(mainContentElement)
       }, 50)
-    } else {
-      resizeObserver.unobserve(mainContentElement)
     }
     return () => {
-      if (!mainContentElement || !resizeObserver) {
-        return
-      }
       resizeObserver.unobserve(mainContentElement)
+      clearTimeout(reObserveTimeout)
     }
   })
 
